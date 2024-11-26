68dce211dd4baa50e6cb382d162a810dd2aae98f
Greg Mooney <gregory.mooney@quantstack.net>
2024-11-26 11:35:22 +0100

    Split local lab and lite demos (#15)

--- demo/jupyter_server_config.py
+++ demo/jupyter_server_config.py
@@ -2,12 +2,12 @@
 c.ServerApp.open_browser = False
 
 # disable the token for easier testing in an IFrame
-c.ServerApp.token = ''
+c.ServerApp.token = ""
 
 # Allow embedding JupyterLab in an IFrame from a specific host
 c.ServerApp.tornado_settings = {
-    'headers': {
-        'Content-Security-Policy': "frame-ancestors 'self' http://localhost:8080"
+    "headers": {
+        "Content-Security-Policy": "frame-ancestors 'self' http://localhost:8080 http://127.0.0.1:8080"
     }
 }
-c.ServerApp.allow_origin = 'http://localhost:8080'
+c.ServerApp.allow_origin = "http://localhost:8080"
